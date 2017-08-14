6f1e14fbcd0b4fce886db34425bef48d9e5730c7
Todor Chakarov <tosheee@abv.bg>
2017-08-14 17:18:28 +0300

    Add admin authorization

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,25 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Auth::routes();
+
+    Route::get('/home', 'HomeController@index');
+
+    //admin
+
+    Route::prefix('admin')->group(function(){
+        Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+        Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+        Route::get('/', 'AdminController@index')->name('admin.dashboard');
+        Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+
+        // Password reset routes
+        Route::post('/password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+        Route::get('/password/reset', 'Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+        Route::post('/password/reset', 'Auth\AdminResetPasswordController@reset');
+        Route::get('/password/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
+    });
